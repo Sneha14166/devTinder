4ef9100daed778b11a2a498e4ba4d996ce685fe2
Sneha <sneha14166@gmail.com>
2025-03-28 02:07:19 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const express=require('express');
 
 const app=express();
-app.use("/hp", (req,res)=>{
-    res.send("homepage");
-})
+
+app.get("/user",(req, res)=>{
+    res.send({firstname:"Sneha", LastName:"Goel"});
+});
+
+app.post("/user",(req,res)=>{
+    console.log("Save the data to the database");
+    res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res)=>{
+    res.send("Deleted successfully");
+});
 
 app.use("/test",(req, res)=>{
     res.send("Hello from the server");
