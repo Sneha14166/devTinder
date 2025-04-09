e86ca55ec9fd5381572b5665d4931b2f84c9ebfc
Sneha <sneha14166@gmail.com>
2025-04-10 00:35:31 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express=require('express');
 
 const app=express();
 
+const {adminAuth}=require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("admin/getAllData", (req,res)=>{
+    res.send("all data sent");
+});
+
 app.get("/user",(req, res)=>{
     res.send({firstname:"Sneha", LastName:"Goel"});
 });
