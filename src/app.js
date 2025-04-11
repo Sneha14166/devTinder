c6210955a9c8257308d6268abd71037b93cbdd54
Sneha <sneha14166@gmail.com>
2025-04-11 22:31:04 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -16,6 +16,56 @@ app.post("/signup", async(req, res)=>{
     }
 });
 
+//get user by email
+app.get("/user", async (req,res)=>{
+    const userEmail=req.body.emailId;
+    try{
+        const user=await User.findOne({emailId:userEmail});
+        if(user.length === 0){
+            res.status(404).send("User not found")
+        }
+        else res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//feed api - get /feed - get all the users from the database
+app.get("/feed", async (req, res)=>{
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//delete api call
+app.delete("/user", async(req, res)=>{
+    const userId=req.body.userId;
+    try{
+        const user=await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch (err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+//update data of the user
+app.patch("/user", async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        await User.findByIdAndUpdate({_id: userId}, data, {runValidators:true,});
+        res.send("User updated successfully");
+    }
+    catch(err){
+        res.status(400).send("Update failed:" + err.message);
+    }
+})
+
 connectDB().then(()=>{
     console.log("database connection established");
     app.listen(7777, ()=>{
