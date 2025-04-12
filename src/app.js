810880536fd5318f2119735512599a388423110b
Sneha <sneha14166@gmail.com>
2025-04-12 14:29:16 +0530

    JWT token auth and cookies

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ const app=express();
 const User=require("./models/user");
 const {validateSignUpData}=require("./utils/validation");
 const bcrypt=require("bcrypt");
+const cookieParser=require("cookie-parser");
+const jwt=require("jsonwebtoken");
+require('dotenv').config();
+const {userAuth}=require("./middlewares/auth");
 
-app.use(express.json());
-
+app.use(express.json()); 
+app.use(cookieParser());
 
 app.post("/signup", async(req, res)=>{
     try{
@@ -43,6 +47,17 @@ app.post("/login", async(req, res)=>{
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if(isPasswordValid){
+
+            //create JWT token
+            const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {expiresIn : "1d"});
+
+            // console.log(token);
+
+            //add the token to cookie and send the response back to the user
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 * 3600000), 
+            });
+
             res.send("Login successful!");
         }
         else{
@@ -54,72 +69,19 @@ app.post("/login", async(req, res)=>{
     }
 });
 
-//get user by email
-app.get("/user", async (req,res)=>{
-    const userEmail=req.body.emailId;
+app.get("/profile", userAuth, async(req, res)=>{
     try{
-        const user=await User.findOne({emailId:userEmail});
-        if(user.length === 0){
-            res.status(404).send("User not found")
-        }
-        else res.send(user);
+    const user=req.user;
+    res.send(user);
     }
     catch(err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("ERROR: " + err.message);
     }
 });
 
-//feed api - get /feed - get all the users from the database
-app.get("/feed", async (req, res)=>{
-    try{
-        const users=await User.find({});
-        res.send(users);
-    }
-    catch(err){
-        res.status(400).send("Something went wrong");
-    }
-});
+app.post("/sendConnectionRequest", async(req, res)=>{
 
-//delete api call
-app.delete("/user", async(req, res)=>{
-    const userId=req.body.userId;
-    try{
-        const user=await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully");
-    }catch (err){
-        res.status(400).send("something went wrong");
-    }
-});
-
-//update data of the user
-app.patch("/user", async(req,res)=>{
-    const userId=req.body.userId;
-    const data=req.body;
-
-    try{
-    const ALLOWED_UPDATES=[
-        "userId", "photoURL", "about", "gender", "age", "skills"
-    ];
-
-    const isUpdateAllowed=Object.keys(data).every((k)=>
-        ALLOWED_UPDATES.includes(k)
-    );
-
-    if(!isUpdateAllowed){
-        throw new Error("Update Not Allowed for this field");
-    }
-
-    if(data?.skills.length>10){
-        throw new Error("Skills cannot be more than 10");
-    }
-
-        const user=await User.findByIdAndUpdate({_id: userId}, data, {runValidators:true,});
-        res.send("User updated successfully");
-    }
-    catch(err){
-        res.status(400).send("Update failed:" + err.message);
-    }
-});
+})
 
 connectDB().then(()=>{
     console.log("database connection established");
